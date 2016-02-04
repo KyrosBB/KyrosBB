e317bdce827ca644dcde2922f4ce1869680c1e4c
Matthew Lutz <gothx2009@gmail.com>
2016-02-03 18:07:45 -0600

    Cleaned up template for index

--- themes/default/forumIndex.php
+++ themes/default/forumIndex.php
@@ -1,24 +1,22 @@
-          <div class='panel panel-default'>
-            <div class='panel-heading'>
-              Topics
-            </div>
-            <ul class='list-group'>
-              <?php
-              foreach($topics as $topic) {
-              ?>
-              <li class='list-group-item'>
-                <div class='row'>
-                  <div class='col-md-7'>
-                    <a href='<?php echo $site_dir; ?>?act=ST&id=<?php echo $topic["id"]; ?>'>
-                      <strong><?php echo $topic["name"]; ?></strong>
-                    </a>
-                  </div>
-                  <div class='col-md-5'>
-                  </div>
-                </div>
-              </li>
-              <?php
-              }
-              ?>
-            </ul>
-          </div>
+<div class='panel panel-default'>
+  <div class='panel-heading'>
+    Topics
+  </div>
+  <ul class='list-group'>
+<?php foreach($topics as $topic) { ?>
+    <li class='list-group-item'>
+      <div class='row'>
+        <div class='col-md-7'>
+          <a href='<?php echo $site_dir; ?>?act=ST&id=<?php echo $topic["id"]; ?>'>
+            <strong><?php echo $topic["name"]; ?></strong>
+          </a>
+          by
+          <?php echo $topic["author"]->username; ?>
+        </div>
+        <div class='col-md-5'>
+        </div>
+      </div>
+    </li>
+<?php } ?>
+  </ul>
+</div>
