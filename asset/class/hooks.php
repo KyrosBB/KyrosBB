4f1cf238be29c0cd585270a46db32c989ade9d56
Matthew Lutz <gothx2009@gmail.com>
2016-02-06 22:44:33 -0600

    Added filter hook array for future use.

--- asset/class/hooks.php
+++ asset/class/hooks.php
@@ -1,6 +1,7 @@
 <?php
   class Hooks {
     private $actions = array();
+    private $filters = array();
     function action($k) {
       if(array_key_exists($k, $this->actions)) {
         foreach($this->actions[$k] as $v) {
