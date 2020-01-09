60e7708542d00a729ebc06f7cfcb579742ebecfc
Cazmat <me@mlutz.us>
2020-01-09 00:05:47 -0600

    Update usersOnline.hook.php

--- hooks/usersOnline.hook.php
+++ hooks/usersOnline.hook.php
@@ -8,21 +8,36 @@
       }
     }
   }
+  function Remove_Session() {
+      global $session, $db;
+      if($session->user->id !== 0) {
+          $sql = "UPDATE users SET activity='0' WHERE id=". $session->user->id;
+          if(!$result = $db->query($sql)) {
+              die($db->error);
+          }
+      }
+  }
   function Sidebar_UoBox() {
     global $db;
-    echo "<div class='panel panel-default'><div class='panel-heading'>Online Users</div><div class='panel-body'>";
+    $c = 0;
+    $html = "<div class='panel panel-default'><div class='panel-heading'>Online Users</div><div class='panel-body'>";
     $timeout = (time() - (60*15));
     if($result = $db->query("SELECT * FROM users WHERE activity > {$timeout}")) {
+      $c = $result->num_rows;
       while($row = $result->fetch_object()) {
         $tmp = new User;
         $tmp->generate($row);
-        echo $tmp->generate_avatar(25);
+        $html .= $tmp->generate_avatar(25);
       }
     } else {
       die("We hit a snag... UOPlugin #1");
     }
-    echo "</div></div>";
+    $html .= "</div></div>";
+    if($c >= 1) {
+        echo $html;
+    }
   }
   $hooks->add_action("session_auth", "Update_Session");
+  $hooks->add_action("session_deauth", "Remove_Session");
   $hooks->add_action("sidebar_end", "Sidebar_UoBox");
 ?>
